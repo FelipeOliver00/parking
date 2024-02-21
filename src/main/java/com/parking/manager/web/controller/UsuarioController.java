ddc7306499c149d8bdc12fdc9c52a6e078f67576
Felipe <felipecastro0208@hotmail.com>
2024-02-21 10:16:13 -0300

    created a new endpoint to find an user by id

--- src/main/java/com/parking/manager/web/controller/UsuarioController.java
+++ src/main/java/com/parking/manager/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.parking.manager.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -23,4 +20,9 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
 }
