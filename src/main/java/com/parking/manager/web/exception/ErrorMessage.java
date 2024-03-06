3cce3e5abba08003dd04d671aa73fa995bcf7d23
Felipe <felipecastro0208@hotmail.com>
2024-03-06 08:36:07 -0300

    created the first request response object

---
+++ src/main/java/com/parking/manager/web/exception/ErrorMessage.java
@@ -0,0 +1,48 @@
+package com.parking.manager.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    private Map<String, String> errors;
+
+    public ErrorMessage(){}
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+    }
+}
